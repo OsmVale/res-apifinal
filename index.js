2a726a538838ae6a7dc2c50960d3411f8b0a007a
OsmVale <ovalenzuelad@miumg.edu.gt>
2021-11-07 12:25:57 -0600

    make it better

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(require('./routes/personas'));
+app.use(require('./routes/maestros'));
+app.use(require('./routes/estudiantes'));
+app.use(require('./routes/security'));
+
+app.get('/', (req,res) =>{
+    res.status(200).send('index');  
+} )
+
+app.listen(app.get('port'), () => {
+    console.log(`Server en puerto ${app.get('port')}`);
+});
+/*https://secure-ocean-51892.herokuapp.com/*/
+/*https://git.heroku.com/secure-ocean-51892.git*/
+/*https://github.com/RonaldRuano/P2-REST-API*/
