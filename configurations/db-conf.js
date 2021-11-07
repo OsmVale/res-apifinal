8e6ed3647a5c12f89994cffb7bb69461fb73e983
OsmVale <ovalenzuelad@miumg.edu.gt>
2021-11-07 12:28:10 -0600

    make

--- configurations/db-conf.js
+++ configurations/db-conf.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host:"en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user:"hetww1jqpqdg154l",
-    password:"qedfjgwyf41e382d",
-    database:"qixzc16b8boysi4k",
+    host:"tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user:"cb2c3cpknjnmccsd",
+    password:"eoxtn8ergot7imfc",
+    database:"k2okijxvw93tpf10",
     multipleStatements: true
   });
 
